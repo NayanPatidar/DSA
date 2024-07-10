711f8efdbce7b6de29c5ad515f06cb4abf7af48b
Nayan Patidar <nayanpatidar28@gmail.com>
2024-07-10 11:18:44 +0530

    Basic Idea of DSA

---
+++ Basics/Basic.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // getline Function
+    // string str;
+    // getline(cin , str);
+    // cout << str;
+
+    // ifelse statement
+    // int age;
+    // cin >> age;
+
+    // if (age > 18)
+    // {
+    //     std::cout << "You are an adult" << std::endl;
+    // } else {
+    //     std::cout << "You are not an adult" << std::endl;
+    // }
+
+    // int marks;
+    // cin >> marks;
+
+    // if (marks < 25)
+    // {
+    //     cout << "F";
+    // } else if (marks <=44)
+    // {
+    //     cout << "E";
+    // } else if (marks <=49)
+    // {
+    //     cout << "D";
+    // }else if (marks <=59)
+    // {
+    //     cout << "C";
+    // }else if ( marks <=79)
+    // {
+    //     cout << "B";
+    // }else if ( marks <=100)
+    // {
+    //     cout << "A";
+    // }
+
+    // String can be changed
+    string str = "Nayan";
+    str[0] = 'M';
+    cout << str << endl;
+    
+     
+
+    return 0;
+}
